1c81659c7bb3ef06e423a8604a1a28726e03b85d
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-05-28 18:49:39 +0530

    updating problem previously stuck

--- May long challenge/chef and bitwise product.py
+++ May long challenge/chef and bitwise product.py
@@ -1,38 +1,61 @@
-import math
-
-
+import random
+def bruteForce(x,y,l,r):
+    maxVal = -1
+    zMax = 0
+    for num in range(l, r+1):
+        temp = F(x,y,num)
+        if temp > maxVal:
+            maxVal = temp
+            zMax = num
+    return zMax
+class Bit:
+    def setBit(self, num, pos):
+        return num|(1<<pos)
+    def unsetBit(self, num, pos):
+        return num&(~(1<<pos))
+    def getBit(self, num, pos):
+        return num&(1<<pos)
 def F(x, y, z):
     return (x&z)*(y&z)
-
 t = int(input())
 for _ in range(t):
     x, y, l, r = map(int, input().rstrip().split())
-    pos = int(math.log2(r))
-    z1 = 0#already less than r
-    z2 = 0#already greater than l
-    while l>>pos&1 == r>>pos&1 and pos>=0:
+    bitObj = Bit()
+    pos = 39
+    while bitObj.getBit(l, pos) == bitObj.getBit(r, pos) and pos>0:
         pos-=1
-    z2 += 1<<pos
     currMax = float("-inf")
+    z = 0
+    possibleValues = [l,r]
     for posForZ1 in range(pos-1, -1, -1):
-        if not l>>posForZ1&1:
-            z1 += 1>>posForZ1#setting the bit to make it grater than l
-        dupZ1 = z1
+        if bitObj.getBit(l, posForZ1):
+            continue
+        z1 = bitObj.setBit(l, posForZ1)
         for rest in range(posForZ1-1, -1, -1):
-            if l>>rest&1 or r>>rest&1:#to minimize the value
-                dupZ1 += 1>>rest
-        value = F(x, y, dupZ1)
-        if value > currMax:
-            currMax = value
+            if bitObj.getBit(x, rest) or bitObj.getBit(y, rest):#to minimize the value
+                z1 = bitObj.setBit(z1, rest)
+            else:
+                z1 = bitObj.unsetBit(z1, rest)
+        possibleValues.append(z1)
     for posForZ2 in range(pos-1, -1, -1):
-        dupZ2 = posForZ2
-#unsetting the bit to make it less than r
+        if not bitObj.getBit(r, posForZ2):
+            continue
+        z2 = bitObj.unsetBit(r, posForZ2)#unsetting the bit to make it less than r
         for rest in range(posForZ2-1, -1, -1):
-            if l>>rest&1 or r>>rest&1:#to minimize the value
-                dupZ2 += 1>>rest
-        value = F(x, y, dupZ2)
+            if bitObj.getBit(x, rest) or bitObj.getBit(y, rest):#to minimize the value
+                z2 = bitObj.setBit(z2, rest)
+            else:
+                z2 = bitObj.unsetBit(z2, rest)
+        possibleValues.append(z2)
+    possibleValues.sort()
+    for num in possibleValues:
+        value = F(x,y, num)
         if value > currMax:
+            z = num
             currMax = value
-    print(currMax)
+    # if z!= bruteForce(x,y,l,r):
+    print(z)
+        # break
 
-
+#
+#
--- May long challenge/triple sort.py
+++ May long challenge/triple sort.py
@@ -1,66 +1,98 @@
-import random
+
+def isLoop(i):
+    if arr[i]!=(i+1) and i == arr[arr[i]-1]-1:
+        return True
+    return False
+def swap(i, j, k):
+    v1 = arr[i]
+    v2 = arr[j]
+    v3 = arr[k]
+    arr[i] = v3
+    hashPositions[v3] = i
+    arr[j] = v1
+    hashPositions[v1] = j
+    arr[k] = v2
+    hashPositions[v2] = k
+def findLoop(i, j):
+    for pos in range(i+1 , j):
+        if isLoop(pos):
+            return pos
+    for pos in range(j+1, len(arr)):
+        if isLoop(pos):
+            return pos
+    return -1
+
 t = int(input())
 for _ in range(t):
     n, k = map(int , input().rstrip().split())
     arr = list(map(int , input().rstrip().split()))
-    # arr = []
-    # n = random.randrange(1,10)
-    # k = random.randrange(1, n+1)
-    hash = {}
-    # for i in range(n):
-    #     num = random.randrange(1,n+1)
-    #     if num not in hash:
-    #         arr.append(num)
-    #         hash[num] = 0
-    #     else:
-    #         while num in hash:
-    #             num = random.randrange(1, n+1)
-    #         arr.append(num)
-    #         hash[num] = 0
-    # print(arr)
-    sortedArr = sorted(arr)
-    time = 0
-    startPos = 0
-    pos1, pos2, pos3 = 0, 0, 0
-    result = []
-    while sortedArr != arr and time<=k:
-        cnt = 0
-        pos = 0
-        while pos < len(arr):
-            if cnt >= 3:
-                break
-            if sortedArr[pos] != arr[pos]:
-                if cnt == 0:
-                    pos1 = pos
-                    cnt+=1
-                elif cnt == 1:
-                    pos2 = pos
-                    cnt += 1
-                else:
-                    pos3 = pos
-                    cnt += 1
-            pos += 1
-        if cnt < 3:
-            break
-        temp = [pos1+1, pos2+1, pos3+1]
-        result.append(temp)
-        v1 = arr[pos1]
-        v2 = arr[pos2]
-        v3 = arr[pos3]
-        arr[pos1] = v3
-        arr[pos2] = v1
-        arr[pos3] = v2
-        if arr[pos1]!=pos1+1:
-            pos = pos1
-        else:
-            pos = pos1+1
-        time += 1
-        print(arr)
-    if arr == sortedArr:
-        print(time)
-        for arr in result:
-            for num in arr:
-                print(num, end=" ")
-            print("")
-    else:
+    # arr = list(range(1, random.randrange(100000)))
+    # random.shuffle(arr)
+    noOfLoops = 0
+
+    hashPositions = {}
+    for pos in range(0, len(arr)):
+        if arr[pos] != pos+1 and isLoop(pos):
+            noOfLoops += 1
+        hashPositions[arr[pos]] = pos
+
+    if noOfLoops % 2 != 0:
         print(-1)
+    else:
+        indexChosen = []
+        queue = []
+        pos = 0
+        cnt = 0
+        check = True
+        while pos < len(arr) and cnt <= k:
+            if arr[pos] != pos + 1:
+                i1 = pos
+                i2 = arr[pos] - 1
+                i3 = 0
+                if not isLoop(i1):
+                    i3 = hashPositions[i1 + 1]
+                else:
+                    queue.append(pos)
+                    pos+=1
+                    continue
+                swap(i1, i2, i3)
+                cnt += 1
+                indexChosen.append([i1 + 1, i2 + 1, i3 + 1])
+            else:
+                pos += 1
+
+        if len(queue)!=0 and cnt < k:
+            pos = 0
+            while pos < len(arr) and len(queue)!= 0 and  cnt <= k:
+                if arr[pos] == pos+1 and pos == queue[0]:
+                    queue.pop(0)
+                elif pos == queue[0]:
+                    queue.pop(0)
+                    i1 = pos
+                    i2 = arr[pos] - 1
+                    i3 = 0
+                    if isLoop(i1):
+                        i3 = findLoop(i1, i2)
+                        tempI = i3
+                        i3 = i2
+                        i2 = tempI
+                        if i2 == -1 or i1 == i2 or i2 == i3 or i1 == i3:
+                            check = False
+                            break
+                    else:
+                        i3 = hashPositions[i1 + 1]
+                    swap(i1, i2, i3)
+                    cnt += 1
+                    indexChosen.append([i1 + 1, i2 + 1, i3 + 1])
+                if arr[pos]!=pos+1:
+                    check = False
+                    break
+                pos+=1
+        length = len(indexChosen)
+        if check and length<=(n//2) :
+            print(length)
+            for tempArr in indexChosen:
+                temp = map(str, tempArr)
+                print(' '.join(temp))
+        else:
+            print(-1)
